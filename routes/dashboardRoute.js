532a7d75c267b0bde17006aa984c93cc30ff2e79
wahyuanang <wahyuanng@students.amikom.ac.id>
2024-10-14 23:42:51 +0700

    menerapkan template engine, crate user menggunakan view engine/ template engine, create route baru, penerapan ejs dan setting ejs, pemanggilan folder css menggunakan ketentuan load express, intinya bermain di app.js, route, controller

--- routes/dashboardRoute.js
+++ routes/dashboardRoute.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const dashboardController = require("../controller/dashboardController");
+const upload = require("../middlewares/uploader");
 
 router.get("/users", dashboardController.userPage);
 router.get("/users/create", dashboardController.createPage);
-router.post("/users/create", dashboardController.createUser);
-// view engine = gak ada put/patch dan delete
+router.post(
+  "/users/create",
+  upload.single("photoProfile"),
+  dashboardController.createUser
+);
 
 module.exports = router;
