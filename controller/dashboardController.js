ac8530e116a1540d34489849ab7c50a4c0391653
wahyuanang <wahyuanng@students.amikom.ac.id>
2024-10-16 00:15:13 +0700

    fix: create user in ejs

--- controller/dashboardController.js
+++ controller/dashboardController.js
@@ -2,174 +2,169 @@ const { User } = require("../models");
 const imagekit = require("../lib/imagekit");
 
 async function userPage(req, res) {
-    try {
-        const users = await User.findAll();
-        console.log(users.data)
-        res.render("users/index", {
-            title: "User Page",
-            users
-        })
-    } catch (error) {
-        res.render("error", {
-            message: error.message
-        })
-    }
+  try {
+    const users = await User.findAll();
+    console.log(users.data);
+    res.render("users/index", {
+      title: "User Page",
+      users,
+    });
+  } catch (error) {
+    res.render("error", {
+      message: error.message,
+    });
+  }
 }
 
 async function createUser(req, res) {
-    const newUser = req.body;
-  
+  const newUser = req.body;
+
+  try {
     let uploadedImage = null;
-  
+
     if (req.file) {
       const file = req.file;
       const split = file.originalname.split(".");
       const ext = split[split.length - 1];
       const filename = `Profile-${Date.now()}.${ext}`;
-  
-      try {
-        uploadedImage = await imagekit.upload({
-          file: file.buffer,
-          fileName: filename,
-        });
-      } catch (uploadError) {
-        console.log("Error uploading image:", uploadError);
-        return res.redirect("/error");
-      }
-    }
-  
-    try {
-      await User.create({
-        ...newUser,
-        photoProfile: uploadedImage ? uploadedImage.url : null,
+
+      uploadedImage = await imagekit.upload({
+        file: file.buffer,
+        fileName: filename,
       });
-  
-      res.redirect("/dashboard/admin/users");
-    } catch (error) {
-      console.log("Error creating user:", error);
-      res.redirect("/error");
     }
+
+    await User.create({
+      ...newUser,
+      photoProfile: uploadedImage ? uploadedImage.url : null,
+    });
+
+    res.redirect("/dashboard/admin/users");
+  } catch (error) {
+    console.log("Error:", error);
+    res.redirect("/error");
   }
+}
 
 async function createPage(req, res) {
-    try {
-        res.render("users/create", {
-            title: "Create page",
-        })
-    } catch (error) {
-        res.render("error", {
-            message: error.message
-        })
-    }
+  try {
+    res.render("users/create", {
+      title: "Create page",
+    });
+  } catch (error) {
+    res.render("error", {
+      message: error.message,
+    });
+  }
 }
 
 // Function for get user data by id
 async function getUserById(req, res) {
-    const id = req.params.id;
-    try {
-        const user = await User.findByPk(id);
-        if (!user) {
-            return res.status(404).json({
-                status: "Failed",
-                message: "Can't find spesific id user",
-                isSuccess: false,
-                data: null,
-            });
-        }
-        res.status(200).json({
-            status: "Success",
-            message: "Successfully obtained user data",
-            isSuccess: true,
-            data: { user },
-        });
-    } catch (error) {
-        res.status(500).json({
-            status: "Failed",
-            message: "Failed to get user data",
-            isSuccess: false,
-            data: null,
-            error: error.message,
-        });
+  const id = req.params.id;
+  try {
+    const user = await User.findByPk(id);
+    if (!user) {
+      return res.status(404).json({
+        status: "Failed",
+        message: "Can't find spesific id user",
+        isSuccess: false,
+        data: null,
+      });
     }
+    res.status(200).json({
+      status: "Success",
+      message: "Successfully obtained user data",
+      isSuccess: true,
+      data: { user },
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "Failed",
+      message: "Failed to get user data",
+      isSuccess: false,
+      data: null,
+      error: error.message,
+    });
+  }
 }
 
 // Function for delete user by id
 async function deleteUserById(req, res) {
-    const id = req.params.id;
-    try {
-        const user = await User.findByPk(id);
-        if (!user) {
-            return res.status(404).json({
-                status: "Failed",
-                message: "Can't find spesific id user",
-                isSuccess: false,
-                data: null,
-            });
-        }
-
-        await user.destroy();
-
-        res.status(200).json({
-            status: "Success",
-            message: "Successfully delete user data",
-            isSuccess: true,
-            data: { user },
-        });
-    } catch (error) {
-        res.status(500).json({
-            status: "Failed",
-            message: "Failed to delete user data",
-            isSuccess: false,
-            data: null,
-            error: error.message,
-        });
+  const id = req.params.id;
+  try {
+    const user = await User.findByPk(id);
+    if (!user) {
+      return res.status(404).json({
+        status: "Failed",
+        message: "Can't find spesific id user",
+        isSuccess: false,
+        data: null,
+      });
     }
+
+    await user.destroy();
+
+    res.status(200).json({
+      status: "Success",
+      message: "Successfully delete user data",
+      isSuccess: true,
+      data: { user },
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "Failed",
+      message: "Failed to delete user data",
+      isSuccess: false,
+      data: null,
+      error: error.message,
+    });
+  }
 }
 
 // Function for update user by id
 async function UpdateUserById(req, res) {
-    const { firstName, lastName, age, phoneNumber } = req.body;
-    const id = req.params.id;
-    try {
-        const user = await User.findByPk(id);
-        if (!user) {
-            return res.status(404).json({
-                status: "Failed",
-                message: "Can't find spesific id user",
-                isSuccess: false,
-                data: null,
-            });
-        }
-
-        user.firstName = firstName;
-        user.lastName = lastName;
-        user.age = age;
-        user.phoneNumber = phoneNumber;
-
-        await user.save();
-
-        res.status(200).json({
-            status: "Success",
-            message: "Successfully update user data",
-            isSuccess: true,
-            data: { user },
-        });
-    } catch (error) {
-        res.status(500).json({
-            status: "Failed",
-            message: "Failed to update user data",
-            isSuccess: false,
-            data: null,
-            error: error.message,
-        });
+  const { firstName, lastName, age, phoneNumber } = req.body;
+  const id = req.params.id;
+  try {
+    const user = await User.findByPk(id);
+    if (!user) {
+      return res.status(404).json({
+        status: "Failed",
+        message: "Can't find spesific id user",
+        isSuccess: false,
+        data: null,
+      });
     }
+
+    user.firstName = firstName;
+    user.lastName = lastName;
+    user.age = age;
+    user.phoneNumber = phoneNumber;
+
+    await user.save();
+
+    res.status(200).json({
+      status: "Success",
+      message: "Successfully update user data",
+      isSuccess: true,
+      data: { user },
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "Failed",
+      message: "Failed to update user data",
+      isSuccess: false,
+      data: null,
+      error: error.message,
+    });
+  }
 }
 
 module.exports = {
-    userPage,
-    createPage,
-    getUserById,
-    deleteUserById,
-    UpdateUserById,
-    createUser,
+  userPage,
+  createPage,
+  getUserById,
+  deleteUserById,
+  UpdateUserById,
+  createUser,
 };
--- routes/dashboardRoute.js
+++ routes/dashboardRoute.js
@@ -5,7 +5,11 @@ const upload = require("../middlewares/uploader");
 
 router.get("/users", dashboardController.userPage);
 router.get("/users/create", dashboardController.createPage);
-router.post("/users/create",upload.single("photo"),dashboardController.createUser);
+router.post(
+  "/users/create",
+  upload.single("photoProfile"),
+  dashboardController.createUser
+);
 // view engine = gak ada put/patch dan delete
 
 module.exports = router;
